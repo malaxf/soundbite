b4110ad04e5117549f7508f80fb84075daefc434
malachi frazier <mjf22802@yahoo.com>
2025-11-13 21:24:34 -0800

    Initial Commit

---
+++ soundbite/soundbiteApp.swift
@@ -0,0 +1,17 @@
+//
+//  soundbiteApp.swift
+//  soundbite
+//
+//  Created by Malachi Frazier on 11/13/25.
+//
+
+import SwiftUI
+
+@main
+struct soundbiteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
